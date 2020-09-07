4493e432b4869ec3174ef408efd35f941679dd53
Jc Dior <jessicaclairdior@gmail.com>
2020-09-07 01:20:47 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -9,3 +9,10 @@ def happy_birthday(birthday_kids)
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
+
+def age_appropriate_birthday(birthday_kids)
+  if birthday_kids.each do |kids_name, age|
+    age <= 12
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end
